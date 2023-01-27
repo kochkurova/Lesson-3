64d0b7e4f86ef3732dd09732f0e54bf208178eaf
Elena <kochkurova.en@gmail.com>
2023-01-28 00:27:22 +0600

    2

--- src/test/java/HWLesson3.java
+++ src/test/java/HWLesson3.java
@@ -2,6 +2,6 @@ import org.junit.jupiter.api.Test;
 public class HWLesson3 {
     @Test
     void homework(){
-        System.out.println("это текст версии 2");
+        System.out.println("это текст версии 2.0");
     }
 }
