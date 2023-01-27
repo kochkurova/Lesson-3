0d1a5d1e2799c2f0f7a1fc092f81a6c9f8dec16f
kochkurova <123259626+kochkurova@users.noreply.github.com>
2023-01-28 01:30:36 +0600

    Update HWLesson3.java

--- src/test/java/HWLesson3.java
+++ src/test/java/HWLesson3.java
@@ -2,6 +2,6 @@ import org.junit.jupiter.api.Test;
 public class HWLesson3 {
     @Test
     void homework(){
-        System.out.println("это текст версии 3252.0");
+        System.out.println("это текст версии 32.0");
     }
 }
